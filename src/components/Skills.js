f57e476c7fff75ff78107a5e7ca1f079f1ad429b
Gwyneth Yuen <gwyneth.yuen@gmail.com>
2022-04-07 13:41:38 -0400

    start skills page

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, ListGroup, Row, Col, Badge, Image, Button } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 class Skills extends Component {
 
@@ -12,7 +14,53 @@ class Skills extends Component {
   render() {
     return(
       <Container fluid className="page-container">
-        <h1>Skills</h1>
+        <Container fluid className="siteHeader d-flex align-items-center">
+          <Button><FontAwesomeIcon fa-border-none icon={faBars} background-color="transparent"/></Button>
+          <Image src={require("../images/header.png")} fluid width="200"></Image>
+        </Container>
+        <Container className="px-4">
+          <h1 className="siteHeaderTitle mb-3">Skills</h1>
+          <Row>
+            <Col>
+              <h2>Programming Languages</h2>
+              <ListGroup variant="flush">
+                <ListGroup.Item className="d-flex justify-content-between align-items-start">
+                  <div>Python</div>
+                  <Badge bg="primary" pill>
+                    14
+                  </Badge>
+                </ListGroup.Item>
+                <ListGroup.Item>Java</ListGroup.Item>
+                <ListGroup.Item>C++</ListGroup.Item>
+                <ListGroup.Item>C</ListGroup.Item>
+                <ListGroup.Item>HTML5</ListGroup.Item>
+                <ListGroup.Item>CSS</ListGroup.Item>
+                <ListGroup.Item>JavaScript</ListGroup.Item>
+              </ListGroup>
+            </Col>
+            <Col>
+              <h2>Libraries/Frameworks</h2>
+              <ListGroup variant="flush">
+                <ListGroup.Item>Bootstrap</ListGroup.Item>
+                <ListGroup.Item>React.js</ListGroup.Item>
+                <ListGroup.Item>Node.js</ListGroup.Item>
+                <ListGroup.Item>MongoDB</ListGroup.Item>
+                <ListGroup.Item>SQL</ListGroup.Item>
+              </ListGroup>
+            </Col>
+            <Col>
+              <h2>Tools/Platforms</h2>
+              <ListGroup variant="flush">
+                <ListGroup.Item>Git</ListGroup.Item>
+                <ListGroup.Item>GitHub</ListGroup.Item>
+                <ListGroup.Item>LaTeX</ListGroup.Item>
+                <ListGroup.Item>Google Drive</ListGroup.Item>
+                <ListGroup.Item>Microsoft Office</ListGroup.Item>
+                <ListGroup.Item>Adobe Creative Suite</ListGroup.Item>
+              </ListGroup>
+            </Col>
+          </Row>
+        </Container>
       </Container>
     );
   }
