d7f017295bd7647b7de9ad354c657d0ea5f0904e
Gwyneth Yuen <gwyneth.yuen@gmail.com>
2022-04-28 01:03:19 -0400

    update about

--- src/components/About.js
+++ src/components/About.js
@@ -15,9 +15,9 @@ class About extends Component {
       <Container fluid className="page-container" id="about">
           <p>
             I am a senior and rising graduate student at Rensselaer Polytechnic Institute with a dual B.S. in
-            Computer Science and Electronic Arts and working towards an M.S. in Information Technology. I am
-            actively seeking internships for Summer 2022 and full-time positions after May 2023. I am also open
-            to part-time positions during the school year from August 2022 to May 2023. I have experience with
+            Computer Science and Electronic Arts and working towards an M.S. in Information Technology. I am an
+            incoming Software Engineer Intern at Twilio. I am open to part-time positions during the 
+            school year from August 2022 to May 2023 and full-time positions after May 2023. I have experience with
             a variety of programming languages including Python, Java, C++, SQL, and JavaScript.
           </p>
         <Contact/>
