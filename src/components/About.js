8c3e7440c4418109e25d90a5cbc4a92c4d17ee71
Gwyneth Yuen <gwyneth.yuen@gmail.com>
2023-12-28 12:17:20 -0500

    portfolio updates

--- src/components/About.js
+++ src/components/About.js
@@ -20,6 +20,13 @@ class About extends Component {
             and a Data Engineering Intern at SimpliSafe for the summer of 2021. I have experience with
             a variety of programming languages including Python, Java, C++, C#, SQL, and JavaScript.
           </p>
+          <p>
+            I am a Technology Analyst at Fiserv, Inc. in Berkeley Heights, NJ.
+            I graduated from Rensselaer Polytechnic Institute with a M.S. in Information Technology and a dual B.S. in
+            Computer Science and Electronic Arts. I was a Software Engineer Intern at Twilio for the summer of 2022
+            and a Data Engineering Intern at SimpliSafe for the summer of 2021. I have experience with
+            a variety of programming languages including Python, Java, C++, C#, SQL, and JavaScript.
+          </p>
         <Contact/>
       </Container>
     );
